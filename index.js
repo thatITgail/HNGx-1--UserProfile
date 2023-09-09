9781fdc79094f3dbd04f31f8ef174ac0a0eb51dc
MichBlack <https://github.com/thatITgail/MultiStepForm>
2023-09-09 03:50:59 -0500

    changed the selectors

--- index.js
+++ index.js
@@ -8,18 +8,21 @@ const weekDays = [
   "Saturday"
 ]
 // Selectors
-const currentDay = document.getElementById("currDay");
+const currentDay = document.querySelector(
+  "[data-testid ='currentDayOfTheWeek']")
 const mnthYr = document.getElementById("mnth-yr");
-const timeInMs = document.getElementById("time")
+const timeInMs = document.querySelector("[data-testid ='currentUTCTime']");
+
 // Date constructor
 const date = new Date();
 let day = weekDays[date.getDay()];
 let mnth = date.getMonth();
 let yr = date.getFullYear();
 let utcTime = date.getUTCMilliseconds();
+
 // Assign to respective selectors
 currentDay.textContent = day
-mnthYr.textContent = `${mnth},${yr}`;
+mnthYr.textContent = `${mnth}, ${yr}`;
 timeInMs.textContent = utcTime;
 
 
